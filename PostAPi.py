d86812d6bc85c31ca417508cb8c7ef7b2a72be36
Clément Cadieux <cadieuxclement@gmail.com>
2022-11-28 14:52:13 -0500

    Progress Made on Api I give up for the day

--- PostAPi.py
+++ PostAPi.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from Schemas import SoundSchema
 
 app = Flask(__name__)
 
@@ -6,6 +7,19 @@ app = Flask(__name__)
 @app.route("/api/sound", methods=["POST"])
 def createSound():
     body = request.json
+    error = SoundSchema().validate(data=body)
+
+    if error:
+        return {"message": "Invalid body"}, 401
+
+    if body["soundAvg"] >= 650:
+        body["detected"] = True
+    else:
+        body["detected"] = False
+
+
+
+    return jsonify(body), 200
 
 
 
