b30852ddca9f2b64f447c4ff90cf0f8f6447219f
DuncanBH <2031593@champlaincollege.qc.ca>
2022-11-30 15:17:19 -0500

    Added PyMongo + DB connection

--- PostAPi.py
+++ PostAPi.py
@@ -1,13 +1,45 @@
-from flask import Flask, request
+import pymongo as pymongo
+from flask import Flask, request, jsonify
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+import datetime as dt
+import os
+
+load_dotenv()
+
+MONGODB_LINK = os.environ.get("MONGODB_LINK")
+MONGODB_USER = os.environ.get("MONGODB_USER")
+MONGODB_PASS = os.environ.get("MONGODB_PASS")
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# connecting to mongodb
+client = pymongo.MongoClient(
+    f"mongodb+srv://{MONGODB_USER}:{MONGODB_PASS}@{MONGODB_LINK}/?retryWrites=true&w=majority",
+    server_api=ServerApi('1'))
+
+# name of database
+db = client.SecurityApp
+
+if 'Motion' not in db.list_collection_names():
+    db.create_collection("Motion",
+                         timeseries={'timeField': 'timestamp', 'metaField': 'sensorId', 'granularity': 'minutes'})
+if 'Sound' not in db.list_collection_names():
+    db.create_collection("Sound",
+                         timeseries={'timeField': 'timestamp', 'metaField': 'sensorId', 'granularity': 'minutes'})
+
+# Testing
+db.Sound.insert_one(
+    {"timestamp": dt.datetime.today().replace(microsecond=0), "sensorId": "test", "value": {"$numberInt": "1010"}}
+)
+print(list(db.Motion.find()))
 
 
 @app.route("/api/sound", methods=["POST"])
 def createSound():
     body = request.json
-
-
 
 
 app.run()
