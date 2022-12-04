a0d8260ae683834b9a72e9e77fdaf434dfad7ce5
Clément Cadieux <cadieuxclement@gmail.com>
2022-12-04 18:03:24 -0500

    Made a dummy data creator which sends a data which should be a detect

--- DummyDataCreator.py
+++ DummyDataCreator.py
@@ -1,13 +1,16 @@
 import unittest
 from PostAPi import app
 
-
 class SendDummyData(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
 
-    def test_send_detected_data(self):
+    def send_detected_data(self):
         response = self.app.post("/api/sound",
                                  headers={"Content-Type": "application/json"},
                                  data={"soundAvg": 700})
+
+
+if __name__ == "__main__":
+    unittest.main()
